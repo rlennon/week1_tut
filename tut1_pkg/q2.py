3ae59815a622c79a5cc22f8aa5010649aebc2c03
R Lennon <ruth.lennon@lyit.ie>
2021-09-24 10:48:27 +0100

    Line of comment added to q2

---
+++ tut1_pkg/q2.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------
+# Low level graphics.
+#
+# (C) 2021 Ruth Lennon, LYIT
+# Available under GNU Public License (GPL)
+# -----------------------------------------------------------
+
+top_icon = "^"
+bottom_icon = "v"
+left_icon = "<"
+right_icon = ">"
+
+print("Title")
+# remember to use *
