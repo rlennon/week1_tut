abbabcca85fa96ea6e53739d47b352251cdafb2b
R Lennon <ruth.lennon@lyit.ie>
2021-09-24 11:13:37 +0100

    Question 3 added

---
+++ tut1_pkg/q3.py
@@ -0,0 +1,20 @@
+# -----------------------------------------------------------
+# Discussion file.
+#
+# (C) 2021 Ruth Lennon, LYIT
+# Available under GNU Public License (GPL)
+# -----------------------------------------------------------
+
+# Consider the type of information and it’s structure for the following items.
+# You are not asked to code anything here. Just consider the information:
+# .	ppsn  # your points of note here....
+# . passport number
+# .	temperature in degrees
+# .	cao number
+# .	Miles travelled per day
+# .	Average collection of exam grades
+# .	File path
+# . Person's middle initial
+# . Biography statement
+# . Book abstract
+# . Mathematical function
