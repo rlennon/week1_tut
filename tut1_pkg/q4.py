422781ce9ba3fd0b8bdebd7965dceefd9e2f00a9
R Lennon <ruth.lennon@lyit.ie>
2021-09-24 11:26:43 +0100

    Q4 added

---
+++ tut1_pkg/q4.py
@@ -0,0 +1,17 @@
+# -----------------------------------------------------------
+# Mathematics examples.
+#
+# (C) 2021 Ruth Lennon, LYIT
+# Available under GNU Public License (GPL)
+# -----------------------------------------------------------
+# For each example add a comment to state what is important in that example
+
+ans_1 = 6 + 8 / 2 – 1
+print(ans_1)
+ans_2 = 1 + 1  - 1 + 2 / 2
+ans_3 = 1.1	+ 5 / 2
+ans_4 = 9.2 / 3
+ans_5 = 9.2 % 3
+ans_6 = 1 * 2 + 3 * 5 % 4
+ans_7 = 1 + 8 % 3 * 2 – 9
+ans_8 = 6 + ( 24 % ( 16 – 11))
