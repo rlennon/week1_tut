5cc230aac723e4549a496068ae256a67acd044fa
R Lennon <ruth.lennon@lyit.ie>
2021-09-24 10:12:56 +0100

    first commit

---
+++ tut1_pkg/q1.py
@@ -0,0 +1,28 @@
+# -----------------------------------------------------------
+# Testing variables.
+#
+# (C) 2021 Ruth Lennon, LYIT
+# Available under GNU Public License (GPL)
+# -----------------------------------------------------------
+
+
+print("\t"*5+"Title")
+print("Hello")
+print('Hello World')
+a = 5
+b = 12.3456789
+c = a+b
+d = "A number "+b
+
+e = a+ \
+  b+ \
+  c
+f = 2
+g = a/f
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
