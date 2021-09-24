944eee7b5ad92c83523238f7d716c4d2ec4e7247
R Lennon <ruth.lennon@lyit.ie>
2021-09-24 11:03:50 +0100

    Question 2 added

--- tut1_pkg/q2.py
+++ tut1_pkg/q2.py
@@ -1,5 +1,6 @@
 # -----------------------------------------------------------
-# Low level graphics.
+# Low level graphics. Create a menu screen with options A, B, C.
+# Do not use loops. Just present the graphic not the business logic.
 #
 # (C) 2021 Ruth Lennon, LYIT
 # Available under GNU Public License (GPL)
